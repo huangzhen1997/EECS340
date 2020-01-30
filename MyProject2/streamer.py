52e24b3515d63889bb8d535918d2c4358b8954f0
Zhen Huang <zhuang38@hawk.iit.edu>
2020-01-29 22:43:34 -0600

    adding proejct 2

---
+++ MyProject2/streamer.py
@@ -0,0 +1,39 @@
+# do not import anything else from loss_socket besides LossyUDP
+from lossy_socket import LossyUDP
+# do not import anything else from socket except INADDR_ANY
+from socket import INADDR_ANY
+
+
+class Streamer:
+    def __init__(self, dst_ip, dst_port,
+                 src_ip=INADDR_ANY, src_port=0):
+        """Default values listen on all network interfaces, chooses a random source port,
+           and does not introduce any simulated packet loss."""
+        self.socket = LossyUDP()
+        self.socket.bind((src_ip, src_port))
+        self.dst_ip = dst_ip
+        self.dst_port = dst_port
+
+    def send(self, data_bytes: bytes):
+        """Note that data_bytes can be larger than one packet."""
+        # Your code goes here!  The code below should be changed!
+
+        # for now I'm just sending the raw application-level data in one UDP payload
+        i=0
+        j=len(data_bytes)
+        if(j>1472):
+            while((j-i)>1472):
+                self.socket.sendto(data_bytes[i:i+1472], (self.dst_ip, self.dst_port))
+                i=i+1472
+            self.socket.sendto(data_bytes[i:j], (self.dst_ip, self.dst_port))
+        else:
+            self.socket.sendto(data_bytes, (self.dst_ip, self.dst_port))
+
+    def recv(self) -> bytes:
+        """Blocks (waits) if no data is ready to be read from the connection."""
+        # your code goes here!  The code below should be changed!
+        
+        # this sample code just calls the recvfrom method on the LossySocket
+        data, addr = self.socket.recvfrom()
+        # For now, I'll just pass the full UDP payload to the app
+        return data
